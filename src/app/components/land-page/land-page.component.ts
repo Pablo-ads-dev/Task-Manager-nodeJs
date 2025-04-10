830ee760aa07a14d84e7a4617a86a963bb4e83da
pablo <pabloadsdev@gmail.com>
2025-04-10 15:51:59 -0300

    Primeiro Commit

---
+++ src/app/components/land-page/land-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-land-page',
+  imports: [RouterOutlet],
+  templateUrl: './land-page.component.html',
+  styleUrl: './land-page.component.css'
+})
+export class LandPageComponent {
+title ="teste";
+}
